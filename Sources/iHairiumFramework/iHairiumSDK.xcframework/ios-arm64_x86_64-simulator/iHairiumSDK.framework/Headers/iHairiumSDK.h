2e397dc1ad737655e9e15bc615b8cfba441003fa
Sergey Bogatenko <shlifff@gmail.com>
2025-01-29 13:41:02 +0300

    initial

---
+++ Sources/iHairiumFramework/iHairiumSDK.xcframework/ios-arm64_x86_64-simulator/iHairiumSDK.framework/Headers/iHairiumSDK.h
@@ -0,0 +1,18 @@
+//
+//  iHairiumSDK.h
+//  iHairiumSDK
+//
+//  Created by Sergey Bogatenko on 26.11.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iHairiumSDK.
+FOUNDATION_EXPORT double iHairiumSDKVersionNumber;
+
+//! Project version string for iHairiumSDK.
+FOUNDATION_EXPORT const unsigned char iHairiumSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iHairiumSDK/PublicHeader.h>
+
+
